78d7a76670c1aebc752b92c28ee449f136dd51f7
Eric <s67766@informatik.htw-dresden.de>
2012-01-25 20:25:34 +0100

    Vertragsanbahnung

--- graph.rb
+++ graph.rb
@@ -3,46 +3,52 @@ require "graphviz"
 
 #require 'graphviz'
 
- g = GraphViz.new( :G, :type => :digraph )
- g.node["shape"] = "box"
+g = GraphViz.new( :G, :type => :digraph )
+g.node["shape"] = "box"
 
- # Create nodes
- anbahnung = g.add_nodes( "Vertragsanbahnung" )
+# Create nodes
+schuldrecht = g.add_nodes( "Schuldrecht" )
 
- vorraus = g.add_nodes("Vorraussetzungen", "shape" => "record", "label" => "{Vorraussetzungen:| Geschäftsfähigkeit §104}")
- wille = g.add_nodes("Willenserklärung", "shape" => "record", "label" => "{Willenserklärung: |
-    geheimer Vorbehalt §116 |
-    Scheingeschäft §117 |
-    Scherzerklärung §118 |
-    Formmangel §125ff |
-    gesetzl. Verbot §134 |
-    Sittenwidrigkeit §138 |
-    Auslegung §133}")
- anfecht = g.add_nodes("Anfechtung", "shape" => "record", "label" => "{Anfechtung: §142 Abs.1 | Irrtum §119 | Übermittlungsfehler §120}")
- anfechtersatz = g.add_nodes("Schadensersatz §122")
- intent = g.add_nodes("Letter of Intent §133 + §157")
- wirk = g.add_nodes("Wirksamwerden §130")
+vorraus = g.add_nodes("Vorraussetzungen", "shape" => "record", "label" => "{Vorraussetzungen:| Geschäftsfähigkeit §104}")
+wille = g.add_nodes("Willenserklärung", "shape" => "record", "label" => "{Willenserklärung: |
+   geheimer Vorbehalt §116 |
+   Scheingeschäft §117 |
+   Scherzerklärung §118 |
+   Formmangel §125ff |
+   gesetzl. Verbot §134 |
+   Sittenwidrigkeit §138 |
+   Auslegung §133}")
+anfecht = g.add_nodes("Anfechtung", "shape" => "record", "label" => "{Anfechtung: §142 Abs.1 | Irrtum §119 | Übermittlungsfehler §120}")
+anfecht_ersatz = g.add_nodes("Schadensersatz §122")
+intent = g.add_nodes("Letter of Intent §133 + §157")
+anbahn = g.add_nodes("Vertragsanbahnung §311 Abs.2")
+anbahn_pflicht = g.add_nodes("Pflichten §241 Abs.2")
+anbahn_schaden = g.add_nodes("Schadensersatz §280 Abs.1 Satz 1")
+wirk = g.add_nodes("Wirksamwerden §130")
 
- annahme = g.add_nodes( "Vertragsannahme" )
- kv = g.add_nodes( "Kaufvertrag" )
- wv = g.add_nodes( "Werkvertrag" )
- dv = g.add_nodes( "Dienstvertrag" )
- mv = g.add_nodes( "Mietvertrag" )
+annahme = g.add_nodes( "Vertragsannahme" )
+kv = g.add_nodes( "Kaufvertrag" )
+wv = g.add_nodes( "Werkvertrag" )
+dv = g.add_nodes( "Dienstvertrag" )
+mv = g.add_nodes( "Mietvertrag" )
 
- # Create edges between the nodes
- g.add_edges( anbahnung, vorraus )
- g.add_edges( vorraus, wille )
- g.add_edges( wille, anfecht )
- g.add_edges( anfecht, anfechtersatz )
- g.add_edges( anfecht, wille )
- g.add_edges( wille, intent)
- g.add_edges( intent, wille)
- g.add_edges( wille, wirk )
- g.add_edges( wirk, annahme )
- g.add_edges( annahme, kv )
- g.add_edges( annahme, wv )
- g.add_edges( annahme, dv )
- g.add_edges( annahme, mv )
+# Create edges between the nodes
+g.add_edges( schuldrecht, vorraus )
+g.add_edges( vorraus, wille )
+g.add_edges( wille, anfecht )
+g.add_edges( anfecht, anfecht_ersatz )
+g.add_edges( anfecht, wille )
+g.add_edges( wille, anbahn)
+g.add_edges( anbahn, anbahn_pflicht)
+g.add_edges( anbahn_pflicht, anbahn_schaden)
+g.add_edges( wille, intent)
+g.add_edges( intent, wille)
+g.add_edges( wille, wirk )
+g.add_edges( wirk, annahme )
+g.add_edges( annahme, kv )
+g.add_edges( annahme, wv )
+g.add_edges( annahme, dv )
+g.add_edges( annahme, mv )
 
- # Generate output image
- g.output( :png => "schuldrecht.png" )
+# Generate output image
+g.output( :png => "schuldrecht.png" )
