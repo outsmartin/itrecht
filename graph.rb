876b727e0c9b1f5b4ec69c4c2df7739aefec66d5
Eric <s67766@informatik.htw-dresden.de>
2012-01-25 18:39:57 +0100

    1st try

--- graph.rb
+++ graph.rb
@@ -1,15 +1,24 @@
+require "rubygems"
 require "graphviz"
 
-require 'graphviz'
+#require 'graphviz'
 
  g = GraphViz.new( :G, :type => :digraph )
 
  # Create two nodes
- hello = g.add_node( "$310" )
- world = g.add_node( "World" )
+ anbahnung = g.add_node( "Vertragsanbahnung" )
+ annahme = g.add_node( "Vertragsannahme" )
+ kv = g.add_node( "Kaufvertrag" )
+ wv = g.add_node( "Werkvertrag" )
+ dv = g.add_node( "Dienstvertrag" )
+ mv = g.add_node( "Mietvertrag" )
 
  # Create an edge between the two nodes
- g.add_edge( hello, world )
+ g.add_edge( anbahnung, annahme )
+ g.add_edge( annahme, kv )
+ g.add_edge( annahme, wv )
+ g.add_edge( annahme, dv )
+ g.add_edge( annahme, mv )
 
  # Generate output image
- g.output( :png => "hello_world.png" )
+ g.output( :png => "schuldrecht.png" )
