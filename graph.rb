0a24c8904e547951f67330fa16637b1645907c36
Eric <s67766@informatik.htw-dresden.de>
2012-01-25 20:08:27 +0100

    Willenserklärung

--- graph.rb
+++ graph.rb
@@ -4,21 +4,45 @@ require "graphviz"
 #require 'graphviz'
 
  g = GraphViz.new( :G, :type => :digraph )
+ g.node["shape"] = "box"
 
- # Create two nodes
- anbahnung = g.add_node( "Vertragsanbahnung" )
- annahme = g.add_node( "Vertragsannahme" )
- kv = g.add_node( "Kaufvertrag" )
- wv = g.add_node( "Werkvertrag" )
- dv = g.add_node( "Dienstvertrag" )
- mv = g.add_node( "Mietvertrag" )
+ # Create nodes
+ anbahnung = g.add_nodes( "Vertragsanbahnung" )
 
- # Create an edge between the two nodes
- g.add_edge( anbahnung, annahme )
- g.add_edge( annahme, kv )
- g.add_edge( annahme, wv )
- g.add_edge( annahme, dv )
- g.add_edge( annahme, mv )
+ vorraus = g.add_nodes("Vorraussetzungen", "shape" => "record", "label" => "{Vorraussetzungen:| Geschäftsfähigkeit §104}")
+ wille = g.add_nodes("Willenserklärung", "shape" => "record", "label" => "{Willenserklärung: |
+    geheimer Vorbehalt §116 |
+    Scheingeschäft §117 |
+    Scherzerklärung §118 |
+    Formmangel §125ff |
+    gesetzl. Verbot §134 |
+    Sittenwidrigkeit §138 |
+    Auslegung §133}")
+ anfecht = g.add_nodes("Anfechtung", "shape" => "record", "label" => "{Anfechtung: §142 Abs.1 | Irrtum §119 | Übermittlungsfehler §120}")
+ anfechtersatz = g.add_nodes("Schadensersatz §122")
+ intent = g.add_nodes("Letter of Intent §133 + §157")
+ wirk = g.add_nodes("Wirksamwerden §130")
+
+ annahme = g.add_nodes( "Vertragsannahme" )
+ kv = g.add_nodes( "Kaufvertrag" )
+ wv = g.add_nodes( "Werkvertrag" )
+ dv = g.add_nodes( "Dienstvertrag" )
+ mv = g.add_nodes( "Mietvertrag" )
+
+ # Create edges between the nodes
+ g.add_edges( anbahnung, vorraus )
+ g.add_edges( vorraus, wille )
+ g.add_edges( wille, anfecht )
+ g.add_edges( anfecht, anfechtersatz )
+ g.add_edges( anfecht, wille )
+ g.add_edges( wille, intent)
+ g.add_edges( intent, wille)
+ g.add_edges( wille, wirk )
+ g.add_edges( wirk, annahme )
+ g.add_edges( annahme, kv )
+ g.add_edges( annahme, wv )
+ g.add_edges( annahme, dv )
+ g.add_edges( annahme, mv )
 
  # Generate output image
  g.output( :png => "schuldrecht.png" )
